ef339208dede1cf42f86acc398808194c2b752c3
IsraelDev <devisrael560@gmail.com>
2024-10-06 23:09:10 -0300

    add: locale.us

--- src/abstraction/Abstraction.java
+++ src/abstraction/Abstraction.java
@@ -4,23 +4,22 @@ import abstraction.exercises.Boleto;
 import abstraction.exercises.FormaPagamento;
 import abstraction.exercises.Pix;
 
+import java.util.Locale;
 import java.util.Scanner;
 
 public class Abstraction {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        scanner.useLocale(Locale.US);
         System.out.println("Escolha uma forma de pagamento.");
         System.out.println("Boleto, Pix ou Cartão de credito.");
         String opcao = scanner.next().toUpperCase();
-        scanner.nextLine();
         switch (opcao){
             case "BOLETO":
                 System.out.println("Insira o codigo do Boleto:");
                 String codigo = scanner.next();
-                scanner.nextLine();
                 System.out.println("Insira o valor a pagar");
                 double valorAPagar = scanner.nextDouble();
-                scanner.nextLine();
                 System.out.println("Insira seu dinheiro");
                 double valorPago = scanner.nextDouble();
                 Boleto boleto = new Boleto(codigo, valorAPagar);
@@ -30,10 +29,8 @@ public class Abstraction {
             case "PIX":
                 System.out.println("Insira o nome, email, ou chave aleatoria.");
                 String destinatario = scanner.next();
-                scanner.nextLine();
                 System.out.println("Insira o valor a pagar");
                 valorAPagar = scanner.nextDouble();
-                scanner.nextLine();
                 System.out.println("Digite 'SIM' para confirmar, e 'NÃO' para cancelar.");
                 String confirm = scanner.next().toUpperCase();
                 Pix pix = new Pix(destinatario, confirm);
